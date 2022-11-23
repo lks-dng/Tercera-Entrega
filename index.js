787898a62c29c965544e048e9ad58db90c783c91
lks-dng <118869122+lks-dng@users.noreply.github.com>
2022-11-23 01:56:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -13,31 +13,31 @@ class Producto {
 
 const productsArray = []
 
-const shampooEquilibrado = new Producto(1,'Shampoo Equilibrado',1200,100)
+const shampooEquilibrado = new Producto(1,'Shampoo Equilibrado',1200, 100)
 productsArray.push(shampooEquilibrado)
-const acondicionador = new Producto(2,'Acondicionador',1500,100)
+const acondicionador = new Producto(2,'Acondicionador',1500, 100)
 productsArray.push(acondicionador)
-const serumCapilar = new Producto(3,'Sérum Capilar',2100,100)
+const serumCapilar = new Producto(3,'Sérum Capilar',2100, 100)
 productsArray.push(serumCapilar)
 
-const jabonExfoliante = new Producto(1,'Jabón Exfoliante',800,100)
+const jabonExfoliante = new Producto(4,'Jabón Exfoliante',800, 100)
 productsArray.push(jabonExfoliante)
-const jabonDeCoco = new Producto(2,'Jabón de Coco',500,100)
+const jabonDeCoco = new Producto(5,'Jabón de Coco',500, 100)
 productsArray.push(jabonDeCoco)
-const jabonDeCalendula = new Producto(3,'Jabón de Calendula',700,100)
+const jabonDeCalendula = new Producto(6,'Jabón de Calendula',700, 100)
 productsArray.push(jabonDeCalendula)
-const jabonDeJazmin = new Producto(4,'Jabón de Jazmin',500,100)
+const jabonDeJazmin = new Producto(7,'Jabón de Jazmin',500, 100)
 productsArray.push(jabonDeJazmin)
-const jabonDeMelon = new Producto(5,'Jabón de Melón',700,100)
+const jabonDeMelon = new Producto(8,'Jabón de Melón',700, 100)
 productsArray.push(jabonDeMelon)
 
-const aceiteEsencialDeMenta = new Producto(1,'Aceite Esencial de Menta',1000,100)
+const aceiteEsencialDeMenta = new Producto(9,'Aceite Esencial de Menta',1000, 100)
 productsArray.push(aceiteEsencialDeMenta)
-const aceiteEsencialDeMirra = new Producto(2,'Aceite Esencial de Mirra',1200,100)
+const aceiteEsencialDeMirra = new Producto(10,'Aceite Esencial de Mirra',1200, 100)
 productsArray.push(aceiteEsencialDeMirra)
-const aceiteEsencialDeRosaMosqueta = new Producto(3,'Aceite Esencial de Rosa Mosqueta',1400,100)
+const aceiteEsencialDeRosaMosqueta = new Producto(11,'Aceite Esencial de Rosa Mosqueta',1400, 100)
 productsArray.push(aceiteEsencialDeRosaMosqueta)
-const aceiteEsencialDeLavanda = new Producto(4,'Aceite Esencial de Lavanda',1600,100)
+const aceiteEsencialDeLavanda = new Producto(12,'Aceite Esencial de Lavanda',1600, 100)
 productsArray.push(aceiteEsencialDeLavanda)
 
 //console.log(productsArray)
@@ -47,11 +47,37 @@ const divProductos = document.querySelector('#divProductos')
 productsArray.forEach(producto => {
 
     divProductos.innerHTML += `
-    <div id="${producto.id}" class="cardProducto">
+    <div id="${producto.ID}" class="card cardProducto">
     <div class="card-body">
     <h5 class="card-title">${producto.Name}</h5>
     <p class="card-text">${producto.Price}</p>
+    <button id="${producto.ID}" class="btn btn-primary">AGREGAR</button>
     </div>
     </div>
     `
+})
+
+// Carrito de Compras
+const carrito = []
+const botonesAgregar = document.querySelectorAll('.btn-primary')
+
+botonesAgregar.forEach(boton=> {
+    boton.onclick = () => {
+        const producto = productsArray.find(prod=>prod.id===parseInt(boton.id))
+        const productoCarrito = {
+            id: producto.id,
+            name: producto.Name,
+            price: producto.price,
+            cantidad: 1
+        }
+
+
+        const indexCarrito = carrito.findIndex(prod=>prod.id === producto.ID)
+        if(indexCarrito === -1){
+            carrito.push(productoCarrito)
+        }else{
+            carrito(indexCarrito).cantidad += 1
+        }
+        console.log(carrito)
+    }
 })
